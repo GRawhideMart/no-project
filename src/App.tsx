60cd2aa8708e0e9d9c92e5c3737d4230157e6bcc
Giulio Mario Martena <giuliomariomartena@gmail.com>
2024-02-19 22:57:41 +0100

    Add landing page image to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import landingPage from "./assets/images/landingpage.png";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl text-green-900">Hello World</h1>
+      <img src={landingPage} alt="ppjp" />
     </>
   );
 }
