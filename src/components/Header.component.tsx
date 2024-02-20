0a6ea31e8499798af2cab4789c1ff140cc5bc95e
Giulio Mario Martena <giuliomariomartena@gmail.com>
2024-02-20 14:58:54 +0100

    Add react-router-dom dependency and update header component

--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -1,6 +1,23 @@
+import { Link } from "react-router-dom";
+
+const HeaderLink = ({ to, children }: { to: string; children: string }) => {
+  return (
+    <Link to={to} className="text-white uppercase px-8">
+      {children}
+    </Link>
+  );
+};
+
 const Header = () => {
   return (
-    <header className="sticky top-0 w-full h-16 bg-emerald-700 z-10 p-4"></header>
+    <header className="sticky top-0 w-full h-16 bg-emerald-700 z-10 p-4">
+      <nav className="flex justify-center items-center">
+        <HeaderLink to="/">Home</HeaderLink>
+        <HeaderLink to="/about">About</HeaderLink>
+        <HeaderLink to="/services">Services</HeaderLink>
+        <HeaderLink to="/contact">Contact</HeaderLink>
+      </nav>
+    </header>
   );
 };
 
