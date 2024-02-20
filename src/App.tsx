805307904b0d20b96732be4d48c26309bd378065
Giulio Mario Martena <giuliomariomartena@gmail.com>
2024-02-20 12:54:44 +0100

    Add Header component and update landing page styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 // import landingPage from "./assets/images/landingpage.png";
+import Header from "./components/Header.component";
 import logo from "/images/logo.png";
 
 function App() {
   return (
     <>
-      <div className="fixed bg-landing-page bg-cover bg-center h-full w-full">
-        <div className="absolute inset-0 bg-gradient-to-t from-black to-neutral-400 opacity-45"></div>
-        <div className="flex flex-col justify-center items-center h-full w-full">
-          <h1 className="hidden">MARTENA GNATOLOGIA & POSTURA</h1>
-          <img
-            src={logo}
-            alt="Martena Gnalogia & Postura"
-            className="w-1/2 md:w-1/3 lg:w-1/4"
-          />
-        </div>
-      </div>
+      <Header />
+      <main>
+        <section className="landing-page overflow-hidden">
+          <div className="relative bg-landing-page bg-cover bg-center h-screen w-screen">
+            <div className="absolute inset-0 bg-gradient-to-t from-black to-amber-500 opacity-35"></div>
+            <div className="flex flex-col justify-center items-center h-full w-full">
+              <img
+                src={logo}
+                alt="Martena Gnalogia & Postura"
+                className="w-1/2 md:w-1/3 lg:w-1/4"
+              />
+              <h1 className="hidden">MARTENA GNATOLOGIA & POSTURA</h1>
+            </div>
+          </div>
+        </section>
+
+        <div>OVERFLOW</div>
+      </main>
     </>
   );
 }
---
+++ src/components/Header.component.tsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <header className="sticky top-0 w-full h-16 bg-emerald-700 z-10 p-4"></header>
+  );
+};
+
+export default Header;
