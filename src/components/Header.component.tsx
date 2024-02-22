3222b8bf54789ae51927dc23257095d8fb6664ff
Giulio Mario Martena <giuliomariomartena@gmail.com>
2024-02-22 22:31:54 +0100

    Refactor header component to use dynamic routes

--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -1,5 +1,12 @@
 import { Link } from "react-router-dom";
 
+const routes = [
+  { route: "/", label: "Home" },
+  { route: "/about", label: "Chi siamo" },
+  { route: "/contact", label: "Contattaci" },
+  { route: "/news", label: "Ultime notizie" },
+];
+
 const HeaderLink = ({ to, children }: { to: string; children: string }) => {
   return (
     <Link to={to} className="text-white uppercase px-8 relative">
@@ -8,14 +15,27 @@ const HeaderLink = ({ to, children }: { to: string; children: string }) => {
   );
 };
 
+const HeaderLinks = ({
+  routes,
+}: {
+  routes: { route: string; label: string }[];
+}) => {
+  return (
+    <div className="flex justify-center items-center">
+      {routes.map((r: { route: string; label: string }, index: number) => (
+        <HeaderLink key={index} to={r.route}>
+          {r.label}
+        </HeaderLink>
+      ))}
+    </div>
+  );
+};
+
 const Header = () => {
   return (
     <header className="flex justify-center items-center sticky top-0 w-full h-16 bg-emerald-800 z-10 p-4 opacity-90">
       <nav className="font-serif">
-        <HeaderLink to="/">Home</HeaderLink>
-        <HeaderLink to="/about">Chi siamo</HeaderLink>
-        <HeaderLink to="/contact">Contattaci</HeaderLink>
-        <HeaderLink to="/news">Ultime notizie</HeaderLink>
+        <HeaderLinks routes={routes} />
       </nav>
     </header>
   );
