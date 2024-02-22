6a41ce58dc16520fe7eaae862a874c343bea5c49
Giulio Mario Martena <giuliomariomartena@gmail.com>
2024-02-22 15:21:47 +0100

    Hotfix: small text on navbar

--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -11,7 +11,7 @@ const HeaderLink = ({ to, children }: { to: string; children: string }) => {
 const Header = () => {
   return (
     <header className="flex justify-center items-center sticky top-0 w-full h-16 bg-emerald-800 z-10 p-4 opacity-90">
-      <nav className="font-serif">
+      <nav className="font-serif text-sm">
         <HeaderLink to="/">Home</HeaderLink>
         <HeaderLink to="/about">Chi siamo</HeaderLink>
         <HeaderLink to="/contact">Contattaci</HeaderLink>
