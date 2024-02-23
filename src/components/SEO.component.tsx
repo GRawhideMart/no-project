2f2a7c3c7bf7b240653467a9b5c61dcd2cc0c237
Giulio Mario Martena <giuliomariomartena@gmail.com>
2024-02-23 12:11:01 +0100

    Update SEO component and index.html

--- src/components/SEO.component.tsx
+++ src/components/SEO.component.tsx
@@ -6,26 +6,49 @@ type SEOProps = {
   name: string;
   type: string;
   imageUrl?: string;
+  // Schema properties
+  schemaType?: string;
+  schemaContext?: unknown;
+
+  // Additional options
+  canonicalUrl?: string;
+  shouldIndex?: boolean;
 };
 
-const SEO = ({ title, description, name, type, imageUrl }: SEOProps) => {
+const SEO = ({ ...props }: SEOProps) => {
+  // Schema.org markup
+  if (props.schemaType && props.schemaContext) {
+    <script type="application/ld+json">
+      {JSON.stringify(props.schemaContext)}
+    </script>;
+  }
+
+  // Canonical link
+  if (props.canonicalUrl) {
+    <link rel="canonical" href={props.canonicalUrl} />;
+  }
+
+  // Robots meta tag (for indexing control)
+  if (!props.shouldIndex) {
+    <meta name="robots" content="noindex" />;
+  }
   return (
     <Helmet>
-      <title>{title}</title>
-      <meta name="description" content={description} />
+      <title>{props.title}</title>
+      <meta name="description" content={props.description} />
 
       {/* Facebook */}
-      <meta property="og:type" content={type} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      {imageUrl && <meta property="og:image" content={imageUrl} />}
+      <meta property="og:type" content={props.type} />
+      <meta property="og:title" content={props.title} />
+      <meta property="og:description" content={props.description} />
+      {props.imageUrl && <meta property="og:image" content={props.imageUrl} />}
 
       {/* Twitter */}
-      <meta name="twitter:creator" content={name} />
-      <meta name="twitter:card" content={type} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      {imageUrl && <meta name="twitter:image" content={imageUrl} />}
+      <meta name="twitter:creator" />
+      <meta name="twitter:card" content={props.type} />
+      <meta name="twitter:title" content={props.title} />
+      <meta name="twitter:description" content={props.description} />
+      {props.imageUrl && <meta name="twitter:image" content={props.imageUrl} />}
 
       {/* ...other conditional meta tags based on otherProps */}
     </Helmet>
